f29f8c36c32d5dabb7f214fa8252e95362df9d39
Huang Kai Chen <foxlovecat1989@gmail.com>
2020-11-18 21:55:10 +0800

    DI - <!-- 集合注入 List/Set/Map -->

--- src/main/java/com/mycompany/springbasic1106/di/Teacher.java
+++ src/main/java/com/mycompany/springbasic1106/di/Teacher.java
@@ -1,11 +1,15 @@
 package com.mycompany.springbasic1106.di;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Teacher {
     
     private String name;
     private List<String> cls;
+    private Set<Student> students;
+    private Map<String, String> subjects;
 
     public Teacher() {
     }
@@ -18,6 +22,31 @@ public class Teacher {
         this.name = name;
         this.cls = cls;
     }
+
+    public Teacher(String name, List<String> cls, Set<Student> students, Map<String, String> subjects) {
+        this.name = name;
+        this.cls = cls;
+        this.students = students;
+        this.subjects = subjects;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+
+    public Map<String, String> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(Map<String, String> subjects) {
+        this.subjects = subjects;
+    }
+    
+    
     public String getName() {
         return name;
     }
@@ -36,7 +65,8 @@ public class Teacher {
 
     @Override
     public String toString() {
-        return "Teacher{" + "name=" + name + ", cls=" + cls + '}';
+        return "Teacher{" + "name=" + name + ", cls=" + cls + ", students=" + students + ", subjects=" + subjects + '}';
     }
 
+    
 }
