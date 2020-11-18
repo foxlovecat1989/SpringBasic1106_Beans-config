8915170987e1b3e7a1dd39f1e7769aaafb0d6ad8
Huang Kai Chen <foxlovecat1989@gmail.com>
2020-11-18 23:40:44 +0800

    DI - <!-- 工廠模式 FactoryBean -->  singleton

--- src/test/java/di/Test_Lotto.java
+++ src/test/java/di/Test_Lotto.java
@@ -17,9 +17,12 @@ public class Test_Lotto {
         Lotto lotto2 = ctx.getBean("lotto", Lotto.class);
         System.out.println(lotto2);
         
-        // The result will be the same (singleton)
-//        Lotto{amount=5, begin=10, end=39, nums=[30, 23, 21, 36, 18]}
-//        Lotto{amount=5, begin=10, end=39, nums=[30, 23, 21, 36, 18]}
+        // The result will be the same (isSingleton() return true)
+        // Lotto{amount=5, begin=10, end=39, nums=[30, 23, 21, 36, 18]}
+        // Lotto{amount=5, begin=10, end=39, nums=[30, 23, 21, 36, 18]}
+        // Opossed, the result will be different (isSingleton() return false)
+        // Lotto{amount=5, begin=10, end=39, nums=[16, 29, 27, 28, 32]}
+        // Lotto{amount=5, begin=10, end=39, nums=[39, 32, 12, 17, 27]}
         ctx.close();
     }
 }
